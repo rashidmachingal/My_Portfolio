5a85806acd011b1c0b21a2b93b00cdaccb4e3d23
Rashid Machingal <rashileocontact@gmail.com>
2023-05-31 05:27:42 +0530

    social icons

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,7 @@
+import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs'
 import MyImage from '../../../public/rashid-machingal.jpeg'
 import Image from "next/image"
+import Link from 'next/link'
 
 export const Hero = () => {
   return (
@@ -8,6 +10,17 @@ export const Hero = () => {
           <div>
            <h2>Hey I am Rashid</h2>
            <h1>Front-End React Web Developer From Malappuram, Kerala</h1>
+           <div className="social_icons" >
+            <Link href="https://instagram.com/rashid.machingal" target="_blank" >
+             <BsInstagram/>
+            </Link>
+            <Link href="https://github.com/rashidmachingal" target="_blank" >
+             <BsGithub/>
+            </Link>
+            <Link href="https://www.linkedin.com/in/rashid-machingal/" target="_blank" >
+             <BsLinkedin/>
+            </Link>
+          </div>
           </div>
         </div>
         <div className="right_side">
