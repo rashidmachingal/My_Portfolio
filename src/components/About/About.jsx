9070c5d4282d76dfc62e167c93914d7e867b4a80
Rashid Machingal <rashileocontact@gmail.com>
2023-07-16 01:58:57 +0530

    about.jsx issue fixed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,19 +1,20 @@
 export const About = () => {
-    return (
-      <div className="about" >
-          <h2>Who is Rashid Machingal?</h2>
-          <div className="para_container" >
-              <p>
-                A front-end React developer from Malappuram, Kerala. I started my web development 
-                journey in 2020 and I obtained all of my skills using freely available resources 
-                on the internet. 
-              </p>
-              <p>
-                I have successfully completed several projects, showcasing my skills in HTML, CSS,
-                JavaScript, React, and more.  Additionally, I'm an aspiring blogger, excited to 
-                share my insights on coding, technologies, and other related topics.
-              </p>
-          </div>
+  return (
+    <div className="about">
+      <h2>Who is Rashid Machingal?</h2>
+      <div className="para_container">
+        <p>
+          A front-end React developer from Malappuram, Kerala. I started my web
+          development journey in 2020 and I obtained all of my skills using
+          freely available resources on the internet.
+        </p>
+        <p>
+          I have successfully completed several projects, showcasing my skills
+          in HTML, CSS, JavaScript, React, and more. Additionally, I&apos;m an
+          aspiring blogger, excited to share my insights on coding,
+          technologies, and other related topics.
+        </p>
       </div>
-    )
-  }
+    </div>
+  );
+};
