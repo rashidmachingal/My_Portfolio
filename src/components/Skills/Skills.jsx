7a2ab3b89b522693175156cab23572c387938ef6
Rashid Machingal <rashileocontact@gmail.com>
2024-12-28 09:29:11 +0530

    page speed test3

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -9,32 +9,8 @@ export const Skills = () => {
       <h2>TECHNOLOGIES</h2>
       <div className="skills_list">
         <div className="skills_box" >
-          <RiReactjsFill color="#58c4dc" size={32} />
-          <span>React Js</span>
-        </div>
-        <div className="skills_box" >
-          <RiNextjsFill color="black" size={32} />
-          <span>Next Js</span>
-        </div>
-        <div className="skills_box" >
-          <SiJavascript color="#f7df1e" style={{ background: "black" }} size={25} />
-          <span>Javascript</span>
-        </div>
-        <div className="skills_box" >
-          <SiTypescript color="#3178c6" size={23} />
-          <span>Typescript</span>
-        </div>
-        <div className="skills_box" >
-          <SiRedis color="#ff4438" size={23} />
-          <span>Redis</span>
-        </div>
-        <div className="skills_box" >
           <SiNginx color="#009639" size={23} />
           <span>Nginx</span>
-        </div>
-        <div className="skills_box" >
-          <LiaGit color="#f05033" size={23} />
-          <span>Git</span>
         </div>
       </div>
     </div>
