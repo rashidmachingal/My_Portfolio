ee154759bad36e90446abef81f148ea2997fa853
Rashid Machingal <rashileocontact@gmail.com>
2024-12-28 09:27:04 +0530

    page speed test2

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -9,28 +9,20 @@ export const Skills = () => {
       <h2>TECHNOLOGIES</h2>
       <div className="skills_list">
         <div className="skills_box" >
-          <RiTailwindCssFill color="#38bdf8" size={23} />
-          <span>Tailwind</span>
+          <RiReactjsFill color="#58c4dc" size={32} />
+          <span>React Js</span>
         </div>
         <div className="skills_box" >
-          <FaHtml5 color="#e34f26" size={23} />
-          <span>HTML</span>
+          <RiNextjsFill color="black" size={32} />
+          <span>Next Js</span>
         </div>
         <div className="skills_box" >
-          <FaCss3 color="#663399" size={23} />
-          <span>CSS</span>
+          <SiJavascript color="#f7df1e" style={{ background: "black" }} size={25} />
+          <span>Javascript</span>
         </div>
         <div className="skills_box" >
-          <FaNodeJs color="#3f8a3d" size={23} />
-          <span>Node Js</span>
-        </div>
-        <div className="skills_box" >
-          <SiExpress color="" size={23} />
-          <span>Express Js</span>
-        </div>
-        <div className="skills_box" >
-          <SiMongodb color="#3f8a3d" size={23} />
-          <span>Mongo DB</span>
+          <SiTypescript color="#3178c6" size={23} />
+          <span>Typescript</span>
         </div>
         <div className="skills_box" >
           <SiRedis color="#ff4438" size={23} />
