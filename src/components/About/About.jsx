766f747a730859a414aec14f927ccdaba0142d1c
Rashid Machingal <rashileocontact@gmail.com>
2024-12-28 07:58:16 +0530

    production error fixed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,10 +4,10 @@ export const About = () => {
       <h2>ABOUT ME</h2>
       <div className="para_container">
         <p>
-          I'm a React/front-end developer from Malappuram, Kerala, with a
+          I&lsquo;m a React/front-end developer from Malappuram, Kerala, with a
           passion for computers that started in my childhood when I loved
           exploring and tinkering with them. My web development journey began in
-          2020, and since then, I've successfully completed several projects,
+          2020, and since then, I&lsquo;ve successfully completed several projects,
           showcasing my various skills
         </p>
       </div>
