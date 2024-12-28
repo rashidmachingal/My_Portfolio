2e89af6f600e9c4231789d587f3e63e3adfd24ce
Rashid Machingal <rashileocontact@gmail.com>
2024-12-28 20:20:14 +0530

    resume added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ export const Hero = () => {
           width={120}
           height={120}
           alt="Picture of Rashid Machingal"
-          title="Profile picture of Rashid Machingal, he is a web developer"
+          title="Profile picture of Rashid Machingal"
         />
         <div>
           <h2>Hey I am Rashid</h2>
@@ -26,6 +26,15 @@ export const Hero = () => {
             </Link>
             <Link href="https://instagram.com/rashid.machingal" target="_blank">
               <BsInstagram style={{color: "#e95950"}} />
+            </Link>
+          </div>
+          <div className='resume' >
+            <Link target='_blank' href="/RashidMachingal-Resume.pdf">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width={15} height={15} >
+  <path fillRule="evenodd" d="M4.5 2A1.5 1.5 0 0 0 3 3.5v13A1.5 1.5 0 0 0 4.5 18h11a1.5 1.5 0 0 0 1.5-1.5V7.621a1.5 1.5 0 0 0-.44-1.06l-4.12-4.122A1.5 1.5 0 0 0 11.378 2H4.5Zm2.25 8.5a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Zm0 3a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Z" clipRule="evenodd" />
+</svg>
+
+            Download Resume
             </Link>
           </div>
         </div>
